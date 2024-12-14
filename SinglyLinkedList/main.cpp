745779de234cc88c02ae70636a91c3342f134a55
Игнатьев Михаил <CYANIMD@gmail.com>
2024-12-14 09:20:47 +0300

    Реализовал оператор вывода в поток в классах singlyLinkedListNode и singlyLinkedList. В последнем написал методы clear(), isEmpty(), а также добавил внеклассные методы reverse(), for_each(), find(), print().

--- SinglyLinkedList/main.cpp
+++ SinglyLinkedList/main.cpp
@@ -2,18 +2,33 @@
 #include "singlyLinkedList.h"
 
 int main() {
-	singlyLinkedList<int> my_list1{};
-	singlyLinkedList<int> my_list2{};
-	my_list1.push_front(-5);
-	auto node1 = my_list1.push_front(-8);
-	my_list1.push_back(-2);
-	auto node2 = my_list1.push_back(0);
-	auto node3 = my_list1.push_back(3);
+	singlyLinkedList<int>* my_list1 = new singlyLinkedList<int>();
+	singlyLinkedListNode<int>* node1 = my_list1->push_front(0);
+	my_list1->push_back(1);
+	my_list1->push_back(1);
+	my_list1->push_back(2);
+	my_list1->push_back(3);
+	my_list1->push_back(5);
+	my_list1->push_back(8);
 
-	my_list1.insert_before(node1, -12);
-	my_list1.insert_before(node2, -1);
-	my_list1.insert_after(node3, 9);
+	singlyLinkedList<int>* my_list2 = new singlyLinkedList<int>();
+	my_list2->push_back(1);
+	my_list2->push_back(1);
+	std::cout << "nullptr:" << (my_list2->insert_before(*node1, 5) == nullptr) << std::endl;
 
-	auto node4 = my_list2.push_back(2);
+	std::cout << my_list2->hasNode(*node1) << std::endl;
 
+
+	std::cout << *my_list1 << std::endl;
+
+	print(*my_list1);
+
+	auto my_list3 = reverse(*my_list1);
+	std::cout << *my_list3;
+
+	auto node2 = find(*my_list1, 10);
+
+	delete my_list1;
+	delete my_list2;
+	delete my_list3;
 }
--- SinglyLinkedList/singlyLinkedList.h
+++ SinglyLinkedList/singlyLinkedList.h
@@ -1,6 +1,9 @@
 ﻿#ifndef __SINGLYLINKEDLIST_
 #define __SINGLYLINKEDLIST_
 
+#include <iostream>
+#include <functional>
+
 //Класс, реализующий "узел линейного односвязного списка".
 template<typename T>
 class singlyLinkedListNode {
@@ -15,6 +18,12 @@ public:
 	singlyLinkedListNode(const T& data) : _data(data), _next(nullptr) {}
 	//Конструктор с входными параметрами.
 	singlyLinkedListNode(T data, singlyLinkedListNode<T>* next) : _data(data), _next(next) {}
+	//Оператор вывода в поток.
+	friend std::ostream& operator<<(std::ostream& os, const singlyLinkedListNode<T>& node) {
+		os << node._data;
+		return os;
+	}
+
 
 	//Деструктор не нужен.
 };
@@ -29,11 +38,16 @@ public:
 	singlyLinkedList() : _first(nullptr) {}
 	//Деструктор.
 	~singlyLinkedList() {
-		while (_first != nullptr) { //Проход по всем узлам и освобождение ресурсов, затрачиваемых на них.
-			singlyLinkedListNode<T>* node = _first;
-			_first = _first->_next;
-			delete node;
+		clear();
+	}
+	//Оператор вывода в поток.
+	friend std::ostream& operator<<(std::ostream& os, const singlyLinkedList<T>& list) {
+		singlyLinkedListNode<T>* node = list._first;
+		while (node != nullptr) {
+			os << node->_data << ' ';
+			node = node->_next;
 		}
+		return os;
 	}
 	//Добавление элемента в начало списка.
 	//Возвращает указатель на добавленный элемент.
@@ -56,27 +70,27 @@ public:
 	}
 	//Удаление первого элемента списка.
 	void remove_first() {
-		if (_first != nullptr) {
-			singlyLinkedListNode<T>* node = _first;
-			_first = _first->_next;
-			delete node;
-		}
+		if (_first == nullptr) return;
+
+		singlyLinkedListNode<T>* node = _first;
+		_first = _first->_next;
+		delete node;
 	}
 	//Удаление последнего элемента списка.
 	void remove_last() {
-		if (_first != nullptr) {
-			if (_first->_next == nullptr) {
-				delete _first;
-				_first = nullptr;
+		if (_first == nullptr) return;
+
+		if (_first->_next == nullptr) {
+			delete _first;
+			_first = nullptr;
+		}
+		else {
+			singlyLinkedListNode<T>* node = _first;
+			while (node->_next->_next != nullptr) { //Поиск предпоследнего элемента.
+				node = node->_next;
 			}
-			else {
-				singlyLinkedListNode<T>* node = _first;
-				while (node->_next->_next != nullptr) { //Поиск предпоследнего элемента.
-					node = node->_next;
-				}
-				delete node->_next;
-				node->_next = nullptr;
-			}
+			delete node->_next;
+			node->_next = nullptr;
 		}
 	}
 	//Возвращает количество узлов списка.
@@ -92,44 +106,100 @@ public:
 	//Вставка элемента после указанного узла.
 	//Возвращает указатель на добавленный элемент.
 	//Если указанного узла нет в списке, то возвращается nullptr.
-	singlyLinkedListNode<T>* insert_after(singlyLinkedListNode<T>* node, const T& value) {
-		if (node == nullptr || _first == nullptr) return nullptr;
+	singlyLinkedListNode<T>* insert_after(singlyLinkedListNode<T>& node, const T& value) {
+		if (_first == nullptr) return nullptr;
 		if (!hasNode(node)) return nullptr;
 
-		singlyLinkedListNode<T>* newNode = new singlyLinkedListNode<T>{ value, node->_next };
-		node->_next = newNode;
+		singlyLinkedListNode<T>* newNode = new singlyLinkedListNode<T>{ value, node._next };
+		node._next = newNode;
 		return newNode;
 	}
 	//Вставка элемента перед указанным узлом.
 	//Возвращает указатель на добавленный элемент.
 	//Если указанного узла нет в списке, то возвращается nullptr.
-	singlyLinkedListNode<T>* insert_before(singlyLinkedListNode<T>* node, T value) {
-		if (node == nullptr || _first == nullptr) return nullptr;
-		if (!hasNode(node)) return nullptr;
+	singlyLinkedListNode<T>* insert_before(const singlyLinkedListNode<T>& node, const T& value) {
+		if (_first == nullptr) return nullptr;
+		//if (!hasNode(node)) return nullptr;
+		//Проверка излишне увеличит время выполнения метода в два раза.
 
-		if (_first == node) return push_front(value);
+		if (_first == &node) return push_front(value);
 
 		singlyLinkedListNode<T>* currentNode = _first;
-		while (currentNode != nullptr && currentNode->_next != node) {
+		while (currentNode != nullptr && currentNode->_next != &node) {
 			currentNode = currentNode->_next;
 		}
 
 		if (currentNode == nullptr) return nullptr;
 
-		singlyLinkedListNode<T>* newNode = new singlyLinkedListNode<T>{ value, node };
+		singlyLinkedListNode<T>* newNode = new singlyLinkedListNode<T>{ value, currentNode->_next };
 		currentNode->_next = newNode;
 		return newNode;
 	}
 	//Проверяет, содержит ли список данный узел.
-	bool hasNode(singlyLinkedListNode<T>* node) {
+	bool hasNode(const singlyLinkedListNode<T>& node) const {
 		singlyLinkedListNode<T>* currentNode = _first;
 		while (currentNode != nullptr) {
-			if (currentNode == node) return true;
+			if (currentNode == &node) return true;
 			currentNode = currentNode->_next;
 		}
 		return false;
 	}
+	//Очистка списка с высвобождением затрачиваемой памяти.
+	void clear() {
+		while (_first != nullptr) { //Проход по всем узлам и освобождение ресурсов, затрачиваемых на них.
+			singlyLinkedListNode<T>* node = _first;
+			_first = _first->_next;
+			delete node;
+		}
+	}
+	//Проверяет, пустой ли список.
+	bool isEmpty() const {
+		return _first == nullptr;
+	}
 };
+//Возвращает реверсированный исходный линейный односвязный список.
+template<typename T>
+singlyLinkedList<T>* reverse(const singlyLinkedList<T>& list) {
+	singlyLinkedList<T>* result = new singlyLinkedList<T>();
+	singlyLinkedListNode<T>* node = list._first;
+	while (node != nullptr) {
+		result->push_front(node->_data);
+		node = node->_next;
+	}
+	return result;
+}
+//Возвращает указатель на первое вхождение в список указанного значения.
+//Если такого вхождения нет, то возвращается nullptr.
+template<typename T>
+singlyLinkedListNode<T>* find(const singlyLinkedList<T>& list, T value) {
+	singlyLinkedListNode<T>* node = list._first;
+	while (node != nullptr) {
+		if (node->_data == value) return node;
+		node = node->_next;
+	}
+	return nullptr;
+}
+//Обход линейного односвязного списка с выполнением заданного метода.
+template<typename T>
+void for_each(const singlyLinkedList<T>& list, void(*f)(T)) {
+	singlyLinkedListNode<T>* node = list._first;
+	while (node != nullptr) {
+		f(node->_data);
+		node = node->_next;
+	}
+}
+//Вывод в консоль узлов линейного односвязного списка и их связей.
+template<typename T>
+void print(const singlyLinkedList<T>& list) {
+	singlyLinkedListNode<T>* node = list._first;
+	while (node != nullptr) {
+		std::cout << node->_data << "->";
+		node = node->_next;
+	}
+	std::cout << "||" << std::endl;
+}
+
+
 
 
 #endif __SINGLYLINKEDLIST_
